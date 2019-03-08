4107866ee4db19e10b3be2ebd15f334429084128
oskarddfa <oskar.pfeffer@gmail.com>
2019-03-08 13:38:04 +0100

    Embedded Graph Plotting Library

---
+++ src/EmbeddedGraphsPlot.jl
@@ -0,0 +1,15 @@
+using LightGraphs
+using GraphPlot
+using Compose
+
+import GraphPlot.gplot
+
+function gplot(g::EmbeddedGraph, args...)
+    loc_x = vertices_loc_x(g)
+    loc_y = vertices_loc_y(g)
+    gplot(g.graph, loc_x, loc_y)
+end
+
+function glpot(g::EmbeddedGraph, loc_x::Array{<:Real}, loc_y::Array{<:Real}, args...)
+    gplot(g.graph, loc_x, loc_y)
+end
