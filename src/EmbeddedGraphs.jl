d118c63b19bca71c452e566a47e7dc6f0327db08
Paul Schultz <pschultz@pik-potsdam.de>
2019-03-08 12:31:41 +0100

    improved include statement

--- src/EmbeddedGraphs.jl
+++ src/EmbeddedGraphs.jl
@@ -1,14 +1,14 @@
 module EmbeddedGraphs
 
     # EmbeddedGraph constructors
-    include(joinpath(dirname(pathof(@__MODULE__)), "EmbeddedGraphBase.jl"))
+    include(joinpath(dirname(@__FILE__), "EmbeddedGraphBase.jl"))
 
     export euclidean, weights, edges, EmbeddedGraph,
             has_edge, has_vertex, inneighbors, ne, nv, outneighbors,
             vertices, is_directed, add_vertex!, rem_vertex!, edgetype, euclidean
 
     # network characteristics
-    include(joinpath(dirname(pathof(@__MODULE__)), "Characteristics.jl"))
+    include(joinpath(dirname(@__FILE__), "Characteristics.jl"))
 
     export detour_indices
 
