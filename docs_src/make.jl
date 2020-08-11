57dd483537bad23de9db42290b6bab69ae886e38
Paul Schultz <22936388+luap-pik@users.noreply.github.com>
2020-08-11 19:58:58 +0200

    Update make.jl

--- docs_src/make.jl
+++ docs_src/make.jl
@@ -5,14 +5,19 @@ using EmbeddedGraphs
 
 makedocs(
     sitename = "EmbeddedGraphs",
-    format = Documenter.HTML(),
-    modules = [EmbeddedGraphs])
+    format = Documenter.HTML(canonical = "fhell.github.io/EmbeddedGraphs.jl/"),
+    modules = [EmbeddedGraphs],
+    authors = "Frank Hellmann, Oskar Pfeffer, Paul Schultz and further contributors.",
+    sitename = "EmbeddedGraphs.jl",
+)
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+
+deploydocs(
+    repo = "github.com/FHell/EmbeddedGraphs.jl.git",
+    target = "build"
+)
 
 cp("docs_src/build", "docs", force=true)
