56c38f22b9f64ffdfedd557bdbe0ef597056b2c6
koulen <koulen@pik-potsdam.de>
2022-04-19 18:54:13 +0200

    removed vs_sorted since we introduced sort! in rem_vertices!

--- src/lightgraph_extensions.jl
+++ src/lightgraph_extensions.jl
@@ -59,8 +59,8 @@ end
 
 """ Removes multiple vertices with given Indices at once"""
 function rem_vertices!(EG::AbstractEmbeddedGraph, vs::AbstractVector{<:Integer}, args...)
-    vs_sorted = sort!(vs, rev=true)    
-    for i in vs_sorted
+    sort!(vs, rev=true)    
+    for i in vs
         rem_vertex!(EG, i, args...)
     end
 end
